1217abf99d8032a4a3811e3c80f42f144f3b594c
feaad <antwi-donkor_f@outlook.com>
2024-10-22 08:17:15 +0100

    Launch page MVP

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Button from "./button";
 
 export default function Home() {
@@ -8,14 +9,24 @@ export default function Home() {
 					<h2 className='text-4xl font-bold '>The Builder Game</h2>
 				</div>
 				<div className='flex justify-center pt-3'>
-					<h5>Choose a theme!</h5>
+					<h5>Choose a theme</h5>
 				</div>
 				<div className='justify-center'>
-					<div className='grid grid-cols-2 gap-5'>
+          <div className='grid grid-cols-2 gap-5'>
+			{/* TODO: Should come from the database */}
 						<Button label='Countries' />
 						<Button label='Animals' />
 					</div>
 				</div>
+					<div className='absolute bottom-20 inset-x-0 '>
+          <div className="flex justify-center">
+            {/* TODO: Link to game page */}
+							<Link href={"/"} className='font-semibold text-xl'>
+								Start Game
+							</Link>
+						</div>
+					</div>
+				
 			</div>
 		</main>
 	);
